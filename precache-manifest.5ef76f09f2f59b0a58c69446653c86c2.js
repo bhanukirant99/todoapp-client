2c934301f5da61caefea40e3f2b2acea3ae58607
bhanukirant99 <bhanukirant99@gmail.com>
2020-08-20 03:42:56 +0530

    Updates

---
+++ precache-manifest.5ef76f09f2f59b0a58c69446653c86c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccde4d13960b9c3d19daf7bd8ea71fb2",
+    "url": "/todoapp-client/index.html"
+  },
+  {
+    "revision": "0313ce29ba8e2d73bf39",
+    "url": "/todoapp-client/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "02ee31cb3e13367f090a",
+    "url": "/todoapp-client/static/css/main.dfd98467.chunk.css"
+  },
+  {
+    "revision": "0313ce29ba8e2d73bf39",
+    "url": "/todoapp-client/static/js/2.9bcdb51d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp-client/static/js/2.9bcdb51d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ee31cb3e13367f090a",
+    "url": "/todoapp-client/static/js/main.22c112a4.chunk.js"
+  },
+  {
+    "revision": "c0b76895123ba40ce3bd",
+    "url": "/todoapp-client/static/js/runtime-main.ae33bd24.js"
+  }
+]);
